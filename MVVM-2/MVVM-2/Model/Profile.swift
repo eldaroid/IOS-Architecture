0f4619cc0a57f485a89d9e1aad5728e6360a9545
eldaroid <eldario10@mail.ru>
2021-03-05 23:55:12 +0300

    MVVM-2

---
+++ MVVM-2/MVVM-2/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM-2
+//
+//  Created by Эльдар Попов on 05.03.2021.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM-2/MVVM-2/View/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  MVVM-2
+//
+//  Created by Эльдар Попов on 05.03.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fullNameLabel: UILabel!
+    
+    @IBOutlet weak var ageLabel: UILabel!
+}
